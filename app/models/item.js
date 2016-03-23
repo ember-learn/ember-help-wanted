d10103fa6a898b1dfed37826e6dcc331008ccff3
David Baker <dbaker@acorncomputersolutions.com>
2016-03-22 22:17:00 -0600

    Added in a few functionality options for working on links

--- app/models/item.js
+++ app/models/item.js
@@ -7,5 +7,21 @@ export default DS.Model.extend({
   category:     DS.attr(),
   notes:        DS.attr(),
   rating:       DS.attr(),
-  workingOn:    DS.attr()
+  workingOn:    DS.attr(),
+
+  workingOnLink: Ember.computed('workingOn', function() {
+
+    let displayValue = this.get('workingOn');
+    if(displayValue[0] === '@') {
+      value = displayValue.substring(1, displayValue.length);
+      let value = Ember.Handlebars.Utils.escapeExpression(value);
+      return Ember.String.htmlSafe(`<a href="https://twitter.com/${value}">${displayValue}</a>`);
+    } else if(displayValue[0] === '#') {
+      value = displayValue.substring(1, displayValue.length);
+      let value = Ember.Handlebars.Utils.escapeExpression(value);
+      return Ember.String.htmlSafe(`<a href="#" title="${value} on Ember Slack">${displayValue}</a>`);
+    } else {
+      return Ember.String.htmlSafe(`${displayValue}`);
+    }
+  })
 });
