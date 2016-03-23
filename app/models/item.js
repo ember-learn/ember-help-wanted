835929bc9cfccede26e79ec9f93033a74163ce5c
David Baker <dbaker@acorncomputersolutions.com>
2016-03-22 22:24:59 -0600

    Improved data options for the working on setup

--- app/models/item.js
+++ app/models/item.js
@@ -11,15 +11,28 @@ export default DS.Model.extend({
 
   workingOnLink: Ember.computed('workingOn', function() {
 
+    // todo: let's clean this up! ;-) possibly move it into a custom transform
+    // https://guides.emberjs.com/v2.4.0/models/defining-models/#toc_custom-transforms
     let displayValue = this.get('workingOn');
-    if(displayValue[0] === '@') {
-      value = displayValue.substring(1, displayValue.length);
-      let value = Ember.Handlebars.Utils.escapeExpression(value);
-      return Ember.String.htmlSafe(`<a href="https://twitter.com/${value}">${displayValue}</a>`);
-    } else if(displayValue[0] === '#') {
-      value = displayValue.substring(1, displayValue.length);
-      let value = Ember.Handlebars.Utils.escapeExpression(value);
-      return Ember.String.htmlSafe(`<a href="#" title="${value} on Ember Slack">${displayValue}</a>`);
+
+    if(displayValue.indexOf(':') !== -1) {
+
+      var [ network, username ] = displayValue.split(':');
+
+      let value = Ember.Handlebars.Utils.escapeExpression(username);
+
+      var networkString = '';
+      if(network === 'github') {
+        networkString = `<a href="https://github.com/${value}" title="${value} on Github">`;
+      }
+      else if(network === 'slack') {
+        networkString = `<a href="#" title="${value} on Ember Slack">`;
+      }
+      else if(network === 'twitter') {
+        networkString = `<a href="https://twitter.com/${value}" title="${value} on Twitter">`;
+      }
+
+      return Ember.String.htmlSafe(`${networkString}${value}</a>`);
     } else {
       return Ember.String.htmlSafe(`${displayValue}`);
     }
