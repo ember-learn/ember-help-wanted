b12b2592e9897d5a5959c82c49f6bcf8267c844d
David Baker <dbaker@acorncomputersolutions.com>
2016-05-11 22:41:37 -0600

    Filter issues to only currently open ones

--- app/routes/items.js
+++ app/routes/items.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('issue');
+    return this.store.query('issue', {
+      orderBy: 'githubData/state',
+      equalTo: 'open'
+    });
   }
 });
