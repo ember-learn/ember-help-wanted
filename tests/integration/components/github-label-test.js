e4220d3f62958850205294785abda7ee51f01ca6
hakilebara <frederic.soumare@gmail.com>
2018-08-23 17:18:53 +0200

    fix and rework github-label test

--- tests/integration/components/github-label-test.js
+++ tests/integration/components/github-label-test.js
@@ -1,24 +1,22 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | github-label', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('label is rendered with a text color opposite to the background color', async function(assert) {
 
-    await render(hbs`{{github-label}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{github-label label="Help Wanted" labelColor="#009800"}}
+      {{github-label label="Help Wanted" labelColor="000000"}}
     `);
-
     assert.equal(this.element.textContent.trim(), 'Help Wanted');
+    assert.equal(find('.github-label').style.color, 'rgb(255, 255, 255)');
+
+    await render(hbs`
+      {{github-label label="Help Wanted" labelColor="FFFFFF"}}
+    `);
+    assert.equal(find('.github-label').style.color, 'rgb(0, 0, 0)');
   });
 });
