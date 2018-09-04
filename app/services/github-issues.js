a8416088bcecea3b2d03241f055ec4d96b82ec3a
Sean Massa <endangeredmassa@gmail.com>
2018-09-04 04:04:13 -0500

    use my fork for issues, for now

--- app/services/github-issues.js
+++ app/services/github-issues.js
@@ -51,7 +51,8 @@ let rfcs = [
 ];
 
 let emberHelpWanted = [
-    { repo: 'ember-learn/ember-help-wanted', labels: 'Help Wanted' }
+    { repo: 'EndangeredMassa/ember-help-wanted', labels: 'help wanted' },
+    { repo: 'EndangeredMassa/ember-help-wanted', labels: 'good first issue' }
 ];
 
 let categoryRepos = { core, learning, community, rfcs, emberHelpWanted };
