c162c2b7a4311c1657a6e1f19729a55bb317c04c
Jerry Nummi <jerrynummi@me.com>
2018-09-16 11:17:30 -0700

    Switch from Moment to Dayjs

--- app/models/github-issue.js
+++ app/models/github-issue.js
@@ -2,7 +2,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
 import { computed } from '@ember/object';
-import moment from 'moment';
+import dayjs from 'dayjs';
 
 export default Model.extend({
   number: attr('number'),
@@ -21,6 +21,6 @@ export default Model.extend({
 
   updatedAtFormatted: computed('updatedAt', function() {
     let updatedAt = this.get('updatedAt');
-    return moment(updatedAt).format('MM-DD-YYYY hh:mm Z');
+    return dayjs(updatedAt).format('MM-DD-YYYY hh:mm Z');
   })
 });
