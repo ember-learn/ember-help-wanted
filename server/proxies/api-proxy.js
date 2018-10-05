bab799d560075a36c7fe586c8ae1dd56dd7de00a
Chris Manson <chris.manson@simplabs.com>
2018-10-05 16:45:48 +0100

    adding an environment variable that allows you to switch to local API

--- server/proxies/api-proxy.js
+++ server/proxies/api-proxy.js
@@ -1,15 +1,28 @@
 /* eslint-env node */
 'use strict';
 
+const httpProxy = require('http-proxy');
 const proxyPath = '/github-issues';
-const target = 'https://ember-help-wanted-server.herokuapp.com';
+let target;
+
+if (process.env.LOCAL_API) {
+  target = 'http://localhost:3000';
+} else {
+  target = 'https://ember-help-wanted-server.herokuapp.com';
+}
 
 module.exports = function(app) {
-  let proxy = require('http-proxy').createProxyServer({
-    headers: {
-      host: 'ember-help-wanted-server.herokuapp.com'
-    }
-  });
+  let proxy;
+
+  if (process.env.LOCAL_API) {
+    proxy = httpProxy.createProxyServer({});
+  } else {
+    proxy = httpProxy.createProxyServer({
+      headers: {
+        host: 'ember-help-wanted-server.herokuapp.com'
+      }
+    });
+  }
 
   proxy.on('error', function(err, req) {
     // eslint-disable-next-line no-console
