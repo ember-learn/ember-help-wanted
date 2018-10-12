61e29ce7c2cd50f80b7d5d97fe029b47ae6bae4c
Sean Massa <endangeredmassa@gmail.com>
2018-10-12 01:50:06 -0500

    update mirage config

--- mirage/config.js
+++ mirage/config.js
@@ -1,8 +1,3 @@
 export default function() {
-
-  this.urlPrefix = 'http://localhost:4200';
-
-  this.get('/github-issues', (schema) => {
-    return schema.githubIssues.all();
-  });
+  this.get('/github-issues', 'githubIssues');
 }
