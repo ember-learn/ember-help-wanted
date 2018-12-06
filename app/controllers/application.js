efe1626c5acb1a4a5c4b9875da14343c0f262bfd
Prasanna Vijayan <prasannavijayan446@gmail.com>
2018-12-06 23:27:05 +0530

    Fixing Spaces and trailin commas

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,32 +2,41 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
+  navLinks: '',
 
-	// Application Header Navlink
-	navLinks: [{
-		type: 'link',
-		href: '/',
-		name: 'Home'
-	},{
-		type: 'link',
-		href: '/core',
-		name: 'Core'
-	},{
-		type: 'link',
-		href: '/rfcs',
-		name: 'RFCs'
-	},{
-		type: 'link',
-		href: '/learning',
-		name: 'Learning'
-	},{
-		type: 'link',
-		href: '/community',
-		name: 'Community'
-	},{
-		type: 'link',
-		href: '/emberHelpWanted',
-		name: 'Ember Help Wanted'
-	} ]
+  init() {
+    this._super(...arguments);
+    this.setupNavLinks();
+  },
 
+  setupNavLinks() {
+    let navlinks = [{
+      type: 'link',
+      href: '/',
+      name: 'Home'
+    }, {
+      type: 'link',
+      href: '/core',
+      name: 'Core'
+    }, {
+      type: 'link',
+      href: '/rfcs',
+      name: 'RFCs'
+    }, {
+      type: 'link',
+      href: '/learning',
+      name: 'Learning'
+    }, {
+      type: 'link',
+      href: '/community',
+      name: 'Community'
+    }, {
+      type: 'link',
+      href: '/emberHelpWanted',
+      name: 'Ember Help Wanted'
+    }];
+
+    this.set('navLinks', navlinks);
+
+  }
 });
