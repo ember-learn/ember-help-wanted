81663607721480b609e9fa33411fbb4a4e582faa
Jared Galanis <jaredgalanis@gmail.com>
2019-02-13 23:20:36 -0500

    Use native GitHub search api to filter repos - repos were being returned that did not have help-wanted issues - additionally, not all repos were being returned by ember-data-github - this fixes this by using the native GitHub api - rather than ember data models, the repos are json is used directly

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,14 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model() {
-    return this.get('store').query('github-repository', { user: 'ember-learn', type: 'all' });
+    return fetch('https://api.github.com/search/repositories?q=user:ember-learn+help-wanted-issues:%3E0+archived:false+&type=Repositories').then(function(response) {
+      return response.json().then(function(repos) {
+        repos.items.forEach((repo) => {
+          repo.displayName = repo.full_name.split('/')[1];
+        });
+        return repos.items;
+      });
+    });
   }
 
 });
