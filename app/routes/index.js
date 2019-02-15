6f7502ba94f859c408bf3afa1b61ead768bb099d
Jared Galanis <jaredgalanis@gmail.com>
2019-02-14 21:30:18 -0500

    Push GitHub JSON payload into ember data store

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,12 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model() {
-    return fetch('https://api.github.com/search/repositories?q=user:ember-learn+help-wanted-issues:%3E0+archived:false+&type=Repositories').then(function(response) {
-      return response.json().then(function(repos) {
-        repos.items.forEach((repo) => {
-          repo.displayName = repo.full_name.split('/')[1];
-        });
-        return repos.items;
+    fetch('https://api.github.com/search/repositories?q=user:ember-learn+help-wanted-issues:%3E0+archived:false+&type=Repositories').then((response) => {
+      response.json().then((repos) => {
+        this.store.pushPayload('github-repository', { githubRepository: repos.items });
       });
     });
+    return this.store.peekAll('github-repository');
   }
 
 });
