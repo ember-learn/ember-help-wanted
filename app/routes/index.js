a0eb3b0420771d6490a3c290847c56c6c5953481
Jared Galanis <jaredgalanis@gmail.com>
2019-02-19 08:54:52 -0500

    Improve syntax of index route fetch

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model() {
-    fetch('https://api.github.com/search/repositories?q=user:ember-learn+help-wanted-issues:%3E0+archived:false').then((response) => {
-      response.json().then((repos) => {
-        this.store.pushPayload('github-repository', { githubRepository: repos.items });
-      });
-    });
+    fetch('https://api.github.com/search/repositories?q=user:ember-learn+help-wanted-issues:%3E0+archived:false')
+      .then((response) => response.json())
+      .then((repos) => this.store.pushPayload('github-repository', { githubRepository: repos.items }));
     return this.store.peekAll('github-repository');
   }
 
