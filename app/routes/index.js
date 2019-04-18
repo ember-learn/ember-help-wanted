483a2c344de805e8e7c7ff8a9b04a87b7465c4a3
Chris Manson <chris@manson.ie>
2019-04-18 08:13:11 +0100

    adding API_HOST to the fetch call for repos

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
+import ENV from 'ember-help-wanted/config/environment';
 
 export default Route.extend({
 
   model() {
-    fetch('/github-repositories')
+    let host = ENV.API_HOST || '';
+
+    fetch(`${host}/github-repositories`)
       .then((response) => response.json())
       .then((repos) => this.store.pushPayload('github-repository', { githubRepository: repos }));
     return this.store.peekAll('github-repository');
