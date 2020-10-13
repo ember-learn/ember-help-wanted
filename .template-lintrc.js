d891d6ba073ed9d65cd8d5954ad785909790bb80
Joan Piedra <joan@joanpiedra.com>
2020-10-13 04:01:05 -0400

    fix `link-rel-noopener` lint rule #176

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -9,7 +9,6 @@ module.exports = {
       The recommended rules are failing. Let's fix the errors
       and remove the rule exceptions one by one.
     */
-    'link-rel-noopener': false,
     'no-action': false,
     'no-curly-component-invocation': false,
     'no-negated-condition': false,
