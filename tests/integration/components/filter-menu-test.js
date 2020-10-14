c840e617b969283ee76dac1164ad88bf6a2d0e20
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2020-10-14 08:31:57 -0500

    Update tests/integration/components/filter-menu-test.js

--- tests/integration/components/filter-menu-test.js
+++ tests/integration/components/filter-menu-test.js
@@ -6,8 +6,29 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | filter-menu', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('buttons were rendered', async function(assert) {
-    await render(hbs`<FilterMenu />`);
-    assert.ok(findAll('[data-test-link]').length > 0);
+  test('renders 11 links', async function(assert) {
+    await render(hbs`
+      <FilterMenu />
+    `);
+
+    const links = findAll('[data-test-link]');
+
+    assert.strictEqual(
+      links.length,
+      11,
+      'We see 11 links.'
+    );
+
+    assert.dom(links[0])
+      .hasText(
+        'adopted-ember-addons',
+        'We see the correct 1st link.'
+      );
+
+    assert.dom(links[links.length - 1])
+      .hasText(
+        'RFCs',
+        'We see the correct last link.'
+      );
   });
 });
