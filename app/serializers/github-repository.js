f0549a54142e57596c239e0c47de6cedce24d60e
ijlee2 <ijlee2@users.noreply.github.com>
2020-11-05 17:32:31 -0600

    Created serializer for github-repository model

---
+++ app/serializers/github-repository.js
@@ -0,0 +1,16 @@
+import RESTSerializer from '@ember-data/serializer/rest';
+import { underscore } from '@ember/string';
+
+export default class GithubRepositorySerializer extends RESTSerializer {
+  keyForAttribute(attr) {
+    return underscore(attr);
+  }
+
+  normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
+    const newPayload = {
+      githubRepositories: payload
+    };
+
+    return super.normalizeFindAllResponse(store, primaryModelClass, newPayload, id, requestType);
+  }
+}
