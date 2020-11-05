80356322f230bd33410671cf76ce53c0e94fa14b
ijlee2 <ijlee2@users.noreply.github.com>
2020-11-05 15:37:08 -0600

    Created getters

--- app/models/github-issue.js
+++ app/models/github-issue.js
@@ -1,11 +1,24 @@
 import Model, { attr } from '@ember-data/model';
 
+const API_URL = 'https://api.github.com/repos/';
+
 export default class GithubIssueModel extends Model {
   @attr('string') htmlUrl;
   @attr() labels;
   @attr('number') number;
-  @attr('string') repositoryHtml;
-  @attr('string') repositoryName;
+  @attr('string') repositoryUrl;
   @attr('string') title;
   @attr('date') updatedAt;
+
+  get repositoryHtml() {
+    const url = this.repositoryUrl ?? '';
+
+    return url.replace(API_URL, 'https://github.com/');
+  }
+
+  get repositoryName() {
+    const url = this.repositoryUrl ?? '';
+
+    return url.replace(API_URL, '');
+  }
 }
---
+++ tests/unit/models/github-issue-test.js
@@ -0,0 +1,34 @@
+import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+
+module('Unit | Model | github issue', function(hooks) {
+  setupTest(hooks);
+
+  test('repositoryHtml returns the GitHub repo\'s URL', function(assert) {
+    const store = this.owner.lookup('service:store');
+
+    const model = store.createRecord('github-issue', {
+      repositoryUrl: 'https://api.github.com/repos/ember-learn/ember-help-wanted'
+    });
+
+    assert.strictEqual(
+      model.repositoryHtml,
+      'https://github.com/ember-learn/ember-help-wanted',
+      'We get the correct value.'
+    );
+  });
+
+  test('repositoryName returns the GitHub repo\'s name', function(assert) {
+    const store = this.owner.lookup('service:store');
+
+    const model = store.createRecord('github-issue', {
+      repositoryUrl: 'https://api.github.com/repos/ember-learn/ember-help-wanted'
+    });
+
+    assert.strictEqual(
+      model.repositoryName,
+      'ember-learn/ember-help-wanted',
+      'We get the correct value.'
+    );
+  });
+});
