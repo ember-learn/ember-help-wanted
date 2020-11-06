ec1fdab25f69d12a776a1603a90f3c54dec47aa8
ijlee2 <ijlee2@users.noreply.github.com>
2020-11-05 18:38:19 -0600

    Created fixture data for testing

---
+++ mirage/data/github-issues.js
@@ -0,0 +1,25 @@
+import adopted_ember_addons from './github-issues/adopted-ember-addons';
+import ember_a11y from './github-issues/ember-a11y';
+import ember_cli from './github-issues/ember-cli';
+import ember_data from './github-issues/ember-data';
+import ember_engines from './github-issues/ember-engines';
+import ember_learn from './github-issues/ember-learn';
+import ember_template_lint from './github-issues/ember-template-lint';
+import emberjs from './github-issues/emberjs';
+import empress from './github-issues/empress';
+import rfcs from './github-issues/rfcs';
+import typed_ember from './github-issues/typed-ember';
+
+export default [
+  ...adopted_ember_addons,
+  ...ember_a11y,
+  ...ember_cli,
+  ...ember_data,
+  ...ember_engines,
+  ...ember_learn,
+  ...ember_template_lint,
+  ...emberjs,
+  ...empress,
+  ...rfcs,
+  ...typed_ember,
+];
