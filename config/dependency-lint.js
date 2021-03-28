4db36e5c3c80ebfd28d338cc369482b86efa7a3c
Isaac Lee <ijlee2@users.noreply.github.com>
2021-03-28 08:23:21 +0200

    Reverted temporary need to allow both versions of ember-data

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -1,8 +1,3 @@
 'use strict';
 
-module.exports = {
-  allowedVersions: {
-    // ember-cli-mirage (4.0.1), ember-data (3.0.1)
-    'ember-inflector': '4.0.1 || 3.0.1',
-  },
-};
+module.exports = {};
