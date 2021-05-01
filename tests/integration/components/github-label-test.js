ac498c36cbda125ebd683464a2e050e83d491d1d
Isaac Lee <ijlee2@users.noreply.github.com>
2021-05-01 09:08:31 +0200

    Localized style for <GithubLabel> component

--- tests/integration/components/github-label-test.js
+++ tests/integration/components/github-label-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { find, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | github-label', function (hooks) {
@@ -15,39 +15,5 @@ module('Integration | Component | github-label', function (hooks) {
     `);
 
     assert.strictEqual(this.element.textContent.trim(), 'Help Wanted');
-  });
-
-  test('label is rendered with provided @backgroundColor', async function (assert) {
-    await render(hbs`
-      <GithubLabel
-        @backgroundColor="000000"
-        @label="Help Wanted"
-      />
-    `);
-
-    assert.strictEqual(
-      find('.github-label').style.backgroundColor,
-      'rgb(0, 0, 0)'
-    );
-  });
-
-  test('label is rendered with a text color opposite to the provided @backgroundColor', async function (assert) {
-    await render(hbs`
-      <GithubLabel
-        @backgroundColor="000000"
-        @label="Help Wanted"
-      />
-    `);
-
-    assert.strictEqual(find('.github-label').style.color, 'rgb(255, 255, 255)');
-
-    await render(hbs`
-      <GithubLabel
-        @backgroundColor="FFFFFF"
-        @label="Help Wanted"
-      />
-    `);
-
-    assert.strictEqual(find('.github-label').style.color, 'rgb(0, 0, 0)');
   });
 });
