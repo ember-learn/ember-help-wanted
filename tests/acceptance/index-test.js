7248a5282f4e1a85513c21a99d674934670c670d
Isaac Lee <ijlee2@users.noreply.github.com>
2021-05-02 15:07:18 +0200

    Added application tests

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,9 +1,9 @@
-import { click, currentURL, find, visit } from '@ember/test-helpers';
+import { click, currentURL, visit } from '@ember/test-helpers';
 import percySnapshot from '@percy/ember';
 import FakeTimers from '@sinonjs/fake-timers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import { setupApplicationTest } from 'ember-qunit';
 import loadDefaultScenario from 'ember-help-wanted/mirage/scenarios/default';
+import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
 module('Acceptance | index', function (hooks) {
@@ -30,28 +30,26 @@ module('Acceptance | index', function (hooks) {
     assert.ok(true);
   });
 
-  test('A a user can visit the index page', async function (assert) {
+  test('A user can visit the index page', async function (assert) {
     await visit('/');
 
-    assert.strictEqual(currentURL(), '/', 'The user is on the index page.');
+    assert.strictEqual(currentURL(), '/', 'The URL is correct.');
 
     assert
       .dom('[data-test-github-repository]')
-      .exists({ count: 17 }, 'The user sees 17 repos with help wanted issues.');
+      .exists({ count: 17 }, 'The user sees 17 GitHub repositories.');
   });
 
   test('Each repository card shows the correct content', async function (assert) {
     await visit('/');
 
-    // Check guides-source repository as a representative
-    const repository = find('[data-test-github-repository="guides-source"]');
-
+    // Check guides-source repository as a sample
     assert
-      .dom(repository)
+      .dom('[data-test-github-repository="guides-source"]')
       .hasText('guides-source Forks: 376', 'The card content is correct.');
 
     assert
-      .dom('a', repository)
+      .dom('[data-test-github-repository="guides-source"] a')
       .hasAttribute(
         'href',
         'https://github.com/ember-learn/guides-source/issues?q=is%3Aissue+is%3Aopen+label%3A%22Help+Wanted%22+sort%3Acreate-date',
@@ -59,21 +57,22 @@ module('Acceptance | index', function (hooks) {
       );
   });
 
-  test('A user can visit the issues page', async function (assert) {
+  test('A user can navigate to the issues page', async function (assert) {
     await visit('/');
     await click('[data-test-link="adopted-ember-addons"]');
 
     assert.strictEqual(
       currentURL(),
       '/adopted-ember-addons',
-      'The user is on the issues page for adopted-ember-addons.'
+      'The URL is correct.'
     );
 
     assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('', 'The user sees the correct value for Keyword.');
+
+    assert
       .dom('[data-test-github-issue]')
-      .exists(
-        { count: 9 },
-        'The user sees 9 help wanted issues in adopted-ember-addons.'
-      );
+      .exists({ count: 9 }, 'The user sees 9 GitHub issues.');
   });
 });
--- tests/acceptance/issues-test.js
+++ tests/acceptance/issues-test.js
@@ -1,10 +1,10 @@
-import { click, currentURL, visit } from '@ember/test-helpers';
+import { click, currentURL, fillIn, visit } from '@ember/test-helpers';
 import percySnapshot from '@percy/ember';
 import FakeTimers from '@sinonjs/fake-timers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import { setupApplicationTest } from 'ember-qunit';
 import loadDefaultScenario from 'ember-help-wanted/mirage/scenarios/default';
-import { module, skip, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { module, test } from 'qunit';
 
 module('Acceptance | issues', function (hooks) {
   setupApplicationTest(hooks);
@@ -30,64 +30,186 @@ module('Acceptance | issues', function (hooks) {
     assert.ok(true);
   });
 
-  test('A user can visit an issues page with help wanted issues', async function (assert) {
-    await visit('/adopted-ember-addons');
+  test('A user can visit the issues page (no filters)', async function (assert) {
+    await visit('/ember-learn');
 
-    assert.strictEqual(
-      currentURL(),
-      '/adopted-ember-addons',
-      'The user is on the issues page for adopted-ember-addons.'
-    );
+    assert.strictEqual(currentURL(), '/ember-learn', 'The URL is correct.');
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('', 'The user sees the correct value for Keyword.');
 
     assert
       .dom('[data-test-github-issue]')
-      .exists(
-        { count: 9 },
-        'The user sees 9 help wanted issues in adopted-ember-addons.'
-      );
+      .exists({ count: 65 }, 'The user sees 65 GitHub issues.');
   });
 
-  test('A user can visit an issues page with no help wanted issues', async function (assert) {
-    await visit('/ember-data');
+  test('A user can visit the issues page (label filter)', async function (assert) {
+    await visit('/ember-learn?label=good%20first%20issue');
 
     assert.strictEqual(
       currentURL(),
-      '/ember-data',
-      'The user is on the issues page for ember-data.'
+      '/ember-learn?label=good%20first%20issue',
+      'The URL is correct.'
     );
 
     assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('', 'The user sees the correct value for Keyword.');
+
+    assert
       .dom('[data-test-github-issue]')
-      .doesNotExist('The user sees 0 help wanted issues in ember-data.');
+      .exists({ count: 15 }, 'The user sees 15 GitHub issues.');
   });
 
-  test('A user can visit another issues page', async function (assert) {
+  test('A user can visit the issues page (title filter)', async function (assert) {
+    await visit('/ember-learn?query=guides');
+
+    assert.strictEqual(
+      currentURL(),
+      '/ember-learn?query=guides',
+      'The URL is correct.'
+    );
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('guides', 'The user sees the correct value for Keyword.');
+
+    assert
+      .dom('[data-test-github-issue]')
+      .exists({ count: 9 }, 'The user sees 9 GitHub issues.');
+  });
+
+  test('A user can visit the issues page (title and label filters)', async function (assert) {
+    await visit('/ember-learn?label=good%20first%20issue&query=guides');
+
+    assert.strictEqual(
+      currentURL(),
+      '/ember-learn?label=good%20first%20issue&query=guides',
+      'The URL is correct.'
+    );
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('guides', 'The user sees the correct value for Keyword.');
+
+    assert
+      .dom('[data-test-github-issue]')
+      .exists({ count: 3 }, 'The user sees 3 GitHub issues.');
+  });
+
+  test('A user can navigate to another issues page', async function (assert) {
     await visit('/adopted-ember-addons');
     await click('[data-test-link="RFCs"]');
 
+    assert.strictEqual(currentURL(), '/RFCs', 'The URL is correct.');
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('', 'The user sees the correct value for Keyword.');
+
+    assert
+      .dom('[data-test-github-issue]')
+      .exists({ count: 17 }, 'The user sees 17 GitHub issues.');
+  });
+
+  test('A user can filter issues by label', async function (assert) {
+    await visit('/ember-learn');
+    await click('[data-test-button="good first issue"]');
+
     assert.strictEqual(
       currentURL(),
-      '/RFCs',
-      'The user is on the issues page for RFCs.'
+      '/ember-learn?label=good%20first%20issue',
+      'The URL is correct.'
     );
 
     assert
-      .dom('[data-test-github-issue]')
-      .exists({ count: 17 }, 'The user sees 17 help wanted issues in RFCs.');
-  });
-
-  skip('A user can filter issues by a label', async function (assert) {
-    await visit('/ember-learn');
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('', 'The user sees the correct value for Keyword.');
 
     assert
       .dom('[data-test-github-issue]')
-      .exists(
-        { count: 65 },
-        'The user sees 65 help wanted issues in ember-learn.'
-      );
+      .exists({ count: 15 }, 'The user sees 15 GitHub issues.');
 
+    await percySnapshot(assert);
+  });
+
+  test('A user can clear the label filter', async function (assert) {
+    await visit('/ember-learn');
+    await click('[data-test-button="good first issue"]');
+    await click('[data-test-button="Clear Filter"]');
+
+    assert.strictEqual(currentURL(), '/ember-learn', 'The URL is correct.');
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('', 'The user sees the correct value for Keyword.');
+
+    assert
+      .dom('[data-test-github-issue]')
+      .exists({ count: 65 }, 'The user sees 65 GitHub issues.');
+  });
+
+  test('A user can filter issues by title', async function (assert) {
+    await visit('/ember-learn');
+    await fillIn('[data-test-field="Keyword"]', 'guides');
+    await click('[data-test-button="Search"]');
+
+    assert.strictEqual(
+      currentURL(),
+      '/ember-learn?query=guides',
+      'The URL is correct.'
+    );
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('guides', 'The user sees the correct value for Keyword.');
+
+    assert
+      .dom('[data-test-github-issue]')
+      .exists({ count: 9 }, 'The user sees 9 GitHub issues.');
+
+    await percySnapshot(assert);
+  });
+
+  test('A user can clear the title filter', async function (assert) {
+    await visit('/ember-learn');
+    await fillIn('[data-test-field="Keyword"]', 'guides');
+    await click('[data-test-button="Search"]');
+    await fillIn('[data-test-field="Keyword"]', '');
+    await click('[data-test-button="Search"]');
+
+    assert.strictEqual(currentURL(), '/ember-learn', 'The URL is correct.');
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('', 'The user sees the correct value for Keyword.');
+
+    assert
+      .dom('[data-test-github-issue]')
+      .exists({ count: 65 }, 'The user sees 65 GitHub issues.');
+  });
+
+  test('A user can filter issues by title and label', async function (assert) {
+    await visit('/ember-learn');
+    await fillIn('[data-test-field="Keyword"]', 'guides');
+    await click('[data-test-button="Search"]');
     await click('[data-test-button="good first issue"]');
 
-    // TODO: Fix the bug in filtering issues
+    assert.strictEqual(
+      currentURL(),
+      '/ember-learn?label=good%20first%20issue&query=guides',
+      'The URL is correct.'
+    );
+
+    assert
+      .dom('[data-test-field="Keyword"]')
+      .hasValue('guides', 'The user sees the correct value for Keyword.');
+
+    assert
+      .dom('[data-test-github-issue]')
+      .exists({ count: 3 }, 'The user sees 3 GitHub issues.');
+
+    await percySnapshot(assert);
   });
 });
