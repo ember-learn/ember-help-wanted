b5b020255184034bf548cd9b57118db8335f7683
Isaac Lee <ijlee2@users.noreply.github.com>
2021-05-02 07:45:05 +0200

    Created {{remove-emoji-shortcode}} helper

---
+++ tests/integration/helpers/remove-emoji-shortcode-test.js
@@ -0,0 +1,54 @@
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Helper | remove-emoji-shortcode', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('returns an empty string when label is undefined', async function (assert) {
+    await render(hbs`
+      <div data-test-output>
+        {{remove-emoji-shortcode}}
+      </div>
+    `);
+
+    assert.dom('[data-test-output]').hasNoText('We see the correct output.');
+  });
+
+  test("returns the label unmodified when the label doesn't include emoji shortcodes", async function (assert) {
+    await render(hbs`
+      <div data-test-output>
+        {{remove-emoji-shortcode "help wanted"}}
+      </div>
+    `);
+
+    assert
+      .dom('[data-test-output]')
+      .hasText('help wanted', 'We see the correct output.');
+  });
+
+  test('removes an emoji shortcode when the label includes them', async function (assert) {
+    await render(hbs`
+      <div data-test-output>
+        {{remove-emoji-shortcode ":pray: help wanted"}}
+      </div>
+    `);
+
+    assert
+      .dom('[data-test-output]')
+      .hasText('help wanted', 'We see the correct output.');
+  });
+
+  test('removes all emoji shortcodes when the label includes them', async function (assert) {
+    await render(hbs`
+      <div data-test-output>
+        {{remove-emoji-shortcode "  :hamster: help  :revolving_hearts:  wanted :pray::pray:  " }}
+      </div>
+    `);
+
+    assert
+      .dom('[data-test-output]')
+      .hasText('help wanted', 'We see the correct output.');
+  });
+});
