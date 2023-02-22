6c2816452c874b529869f15d654ce91111c32f0d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-02-22 11:45:14 -0500

    Upgrade addons

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,8 +2,10 @@
 
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
+  throwOnUnhandled: true,
   workflow: [
     { handler: 'silence', matchId: 'ember.component.reopen' },
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
+    { handler: "silence", matchId: "ember-cli-mirage-config-routes-only-export" }
   ],
 };
