bb07c97516119a9679ecb2d977ae76cfdda83f86
Katie Gengler <katie@kmg.io>
2026-05-06 10:41:25 -0400

    Generate new app

---
+++ app/templates/application.gjs
@@ -0,0 +1,8 @@
+import { pageTitle } from 'ember-page-title';
+import { WelcomePage } from 'ember-welcome-page';
+
+<template>
+  {{pageTitle "EmberHelpWanted"}}
+
+  {{outlet}}
+</template>
---
+++ tests/test-helper.js
@@ -0,0 +1,16 @@
+import '@warp-drive/ember/install';
+import Application from 'ember-help-wanted/app';
+import config from 'ember-help-wanted/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
+
+export function start() {
+  setApplication(Application.create(config.APP));
+
+  setup(QUnit.assert);
+  setupEmberOnerrorValidation();
+
+  qunitStart();
+}
